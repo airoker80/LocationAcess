6822eb7c569a7579da15742af09c79947eaa2293
airoker80 <sameerairoker@gmail.com>
2017-01-27 12:59:27 +0545

    location finder

---
+++ app/src/main/java/com/location/xclusive_pc/locationmanager/MyLocation.java
@@ -0,0 +1,62 @@
+package com.location.xclusive_pc.locationmanager;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationListener;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by XCLUSIVE-PC on 1/27/2017.
+ */
+
+public class MyLocation extends AppCompatActivity{
+
+    Button btnShowLocation;
+
+    // GPSTracker class
+    LocationAcess gps;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new LocationAcess(MyLocation.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+    }
+
+}
